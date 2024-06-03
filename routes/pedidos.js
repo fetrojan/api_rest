d166f587b78001248126efcd285afbe88f34cfe9
fetrojan <felipetrojan@hotmail.com>
2024-06-03 16:58:00 -0300

    Criação das rotas + coluna imagem

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,37 +1,105 @@
-const express = require('express')
-const router = express.Router()
-
+const express = require("express");
+const router = express.Router();
+const pool = require("../postgres");
 
 //RETORNA TODOS OS PEDIDOS
-router.get('/', (req, res, next) => {
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query(`
+                SELECT  pedidos.id_pedido,
+                        pedidos.quantidade,
+                        pedidos.id_produto,
+                        produtos.nome,
+                        produtos.preco
+                FROM pedidos
+            INNER JOIN produtos
+                ON produtos.id_produto = pedidos.id_produto;`);
+    const quantidade = result.rows.length;
     res.status(200).send({
-        mensamge: 'Retorna os pedidos'
-    })
-})
-
+      mensagem: "Aqui estão todos os pedidos:",
+      quantidade_pedidos: quantidade,
+      produtos: result.rows,
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+      response: null,
+    });
+  }
+});
 
 //INSERE UM PEDIDO
-router.post('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Insere um novo pedido'
-    })
-})
+router.post("/", async (req, res) => {
+  const { id_produto, quantidade } = req.body;
 
+  try {
+    const result = await pool.query(
+      "INSERT INTO pedidos (id_produto, quantidade) VALUES ($1, $2) RETURNING *",
+      [id_produto, quantidade]
+    );
+    res.status(201).send({
+      mensagem: "Pedido inserido com sucesso",
+      pedido: result.rows[0],
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+      response: null,
+    });
+  }
+});
 
 //RETORNA OS DADOS DE UM PEDIDO ESPECIFICO
-router.get('/:id_pedido', (req, res, next) => {
-    const id = req.params.id_pedido
-    res.status(200).send({
-        mensagem: 'Detalhes do Pedido',
-        id_pedido: id
-    })
-})
+router.get("/:id_pedido", async (req, res) => {
+  const idPedido = req.params.id_pedido;
+  try {
+    const result = await pool.query(
+      "SELECT * FROM pedidos WHERE id_pedido = $1",
+      [idPedido]
+    );
+    if (result.rows.length > 0) {
+      res.status(200).send({
+        mensagem: "Pedido encontrado",
+        pedido: result.rows[0],
+      });
+    } else {
+      res.status(404).send({
+        mensagem: "Pedido não encontrado",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+      response: null,
+    });
+  }
+});
 
 //DELETA UM PEDIDO
-router.delete('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Pedido excluido'
-    })
-})
+router.delete("/", async (req, res) => {
+  const idPedido = req.params.id_pedido;
+
+  try {
+    const result = await pool.query(
+      " DELETE FROM pedidos WHERE id_pedido = $1 RETURNING *",
+      [idPedido]
+    );
+    if (result.rowCount > 0) {
+      res.status(200).send({
+        mensagem: "Pedido excluido com sucesso",
+        pedido: result.rows[0],
+      });
+    } else {
+      res.status(404).send({
+        mensagem: "Pedido não encontrado",
+      });
+    }
+  } catch (erro) {
+    res.status(500).send({
+      error: error.message,
+      response: null,
+    });
+  }
+});
 
 module.exports = router;
--- routes/produtos.js
+++ routes/produtos.js
@@ -1,51 +1,171 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const pool = require("../postgres");
+const multer = require("multer");
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+        cb(null, true);
+    } else {
+        cb(null, false)
+    }
+}
+
+const upload = multer({ 
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 //RETORNA TODOS OS PRODUTOS
-router.get('/', (req, res, next) => {
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM produtos");
+    const quantidade = result.rows.length;
     res.status(200).send({
-        mensamge: 'Retorna todos os produtos'
-    })
-})
-
+      mensagem: "Aqui estão todos os produtos:",
+      quantidade_produtos: quantidade,
+      produtos: result.rows,
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+      response: null,
+    });
+  }
+});
 
 //INSERE UM PRODUTO
-router.post('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Insere um novo produto'
-    })
-})
+router.post("/", upload.single("produto_imagem"), async (req, res) => {
+  console.log(req.file);
+  const { nome, preco } = req.body;
+  const imagem_produto = req.file.path
 
+  try {
+    const result = await pool.query(
+      "INSERT INTO produtos (nome, preco, imagem_produto) VALUES ($1, $2, $3) RETURNING *",
+      [nome, preco, imagem_produto]
+    );
+    res.status(201).send({
+      mensagem: "Produto inserido com sucesso",
+      produto: result.rows[0],
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+      response: null,
+    });
+  }
+});
 
 //RETORNA OS DADOS DE UM PRODUTO ESPECIFICO
-router.get('/:id_produto', (req, res, next) => {
-    const id = req.params.id_produto
-    if(id == 'especial') {
-        res.status(200).send({
-            mensagem: 'Você descobriu o ID especial',
-            id_produto: id
-        })
-    } else {
-        res.status(200).send({
-            mensagem: 'Você passou um ID'
-        })
-    }
 
-})
+router.get("/:id_produto", async (req, res) => {
+  const idProduto = req.params.id_produto;
+  try {
+    const result = await pool.query(
+      "SELECT * FROM produtos WHERE id_produto = $1",
+      [idProduto]
+    );
+    if (result.rows.length > 0) {
+      res.status(200).send({
+        mensagem: "Produto encontrado",
+        produto: result.rows[0],
+      });
+    } else {
+      res.status(404).send({
+        mensagem: "Produto não encontrado",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+      response: null,
+    });
+  }
+});
 
 //ALTERA UM PRODUTO
-router.patch('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Produto Alterado'
-    })
-})
+router.patch("/:id_produto", async (req, res) => {
+  const idProduto = req.params.id_produto;
+  const { nome, preco } = req.body;
+
+  let updateQuery = "UPDATE produtos SET";
+  const updateValues = [];
+  let counter = 1;
+
+  if (nome) {
+    updateQuery += ` nome = $${counter},`;
+    updateValues.push(nome);
+    counter++;
+  }
+
+  if (preco) {
+    updateQuery += ` preco = $${counter},`;
+    updateValues.push(preco);
+    counter++;
+  }
+
+  updateQuery = updateQuery.slice(0, -1);
+  updateQuery += ` WHERE id_produto = $${counter} RETURNING *`;
+  updateValues.push(idProduto);
+
+  try {
+    const result = await pool.query(updateQuery, updateValues);
+    if (result.rowCount > 0) {
+      res.status(200).send({
+        mensagem: "Produto alterado com sucesso!",
+        produto: result.rows[0],
+      });
+    } else {
+      res.status(404).send({
+        mensagem: "Produto não encontrado",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      error: error.message,
+      response: null,
+    });
+  }
+});
 
 //DELETA UM PRODUTO
-router.delete('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Produto Excluido'
-    })
-})
+router.delete("/:id_produto", async (req, res) => {
+  const idProduto = req.params.id_produto;
+
+  try {
+    const result = await pool.query(
+      " DELETE FROM produtos WHERE id_produto = $1 RETURNING *",
+      [idProduto]
+    );
+    if (result.rowCount > 0) {
+      res.status(200).send({
+        mensagem: "Produto excluido com sucesso",
+        produto: result.rows[0],
+      });
+    } else {
+      res.status(404).send({
+        mensagem: "Produto não encontrado",
+      });
+    }
+  } catch (erro) {
+    res.status(500).send({
+      error: error.message,
+      response: null,
+    });
+  }
+});
 
 module.exports = router;
